6c256e19be2866935422a877e06799cef6314f55
Farrukh <farrukhkhan30@hotmail.com>
2019-02-23 15:02:34 +0500

    first commit

---
+++ Default.aspx
@@ -0,0 +1,66 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"/>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:GridView CssClass="table table-bordered table-light table-hover" ID="GridView1" runat="server">
+        </asp:GridView>
+        <table  class="table table-danger table-hover">
+            <tr>
+                <td rowspan="4">
+                    <asp:ListBox ID="ListBox1" runat="server" AutoPostBack="True" CssClass="form-control" OnSelectedIndexChanged="ListBox1_SelectedIndexChanged"></asp:ListBox>
+                </td>
+                <td>
+                    Student Name
+                </td>
+                <td>
+                    <asp:TextBox CssClass="form-control" ID="txtStudName" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>
+                    Student Email
+                </td>
+                <td>
+                    <asp:TextBox CssClass="form-control" TextMode="Email" ID="txtStudEmail" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>
+                    Student Contact No
+                </td>
+                <td>
+                    <asp:TextBox CssClass="form-control" ID="txtContactNo" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>
+                    Student Address
+                </td>
+                <td>
+                    <asp:TextBox CssClass="form-control" TextMode="MultiLine" ID="txtStudAddress" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Button CssClass="btn badge-dark" ID="btnDelete" runat="server" Text="Delete Record" OnClick="btnDelete_Click" />
+                </td>
+                <td></td>
+                <td>
+                    <asp:Button CssClass="btn badge-dark" ID="btnAdd" runat="server" Text="Add Student" OnClick="btnAdd_Click" /></td>
+                <td>
+                    <asp:Button CssClass="btn badge-dark" ID="btnUpdate" runat="server" Text="Update Student" OnClick="btnUpdate_Click" /></td>
+            </tr>
+        </table>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack) 
+        refreshPage();
+
+        
+    }
+
+    private void refreshPage()
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source=LAPTOP-35OT9IE8\SQLEXPRESS;Initial Catalog=webeng;Integrated Security=True");
+
+        DataSet ds = new DataSet();
+
+        SqlDataAdapter adpt = new SqlDataAdapter("Select * from Student", conn);
+
+        adpt.Fill(ds, "Student");
+
+        GridView1.DataSource = ds.Tables["Student"];
+        GridView1.DataBind();
+
+        ListBox1.DataSource = ds.Tables["Student"];
+        ListBox1.DataTextField = "Name";
+        ListBox1.DataValueField = "Id";
+        ListBox1.DataBind();
+    }
+    protected void btnAdd_Click(object sender, EventArgs e)
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source=LAPTOP-35OT9IE8\SQLEXPRESS;Initial Catalog=webeng;Integrated Security=True");
+
+        DataSet ds = new DataSet();
+
+        SqlDataAdapter adpt = new SqlDataAdapter("Select * from Student", conn);
+
+        adpt.Fill(ds, "Student");
+
+        DataRow dr = ds.Tables["Student"].NewRow();
+        dr["Name"] = txtStudName.Text;
+        dr["Email"] = txtStudEmail.Text;
+        dr["Contactno"] = txtContactNo.Text;
+        dr["Address"] = txtStudAddress.Text;
+
+        ds.Tables["Student"].Rows.Add(dr);
+
+        SqlCommandBuilder buil = new SqlCommandBuilder(adpt);
+        adpt.Update(ds, "Student");
+        refreshPage();
+        GridView1.DataBind();
+
+    }
+    protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source=LAPTOP-35OT9IE8\SQLEXPRESS;Initial Catalog=webeng;Integrated Security=True");
+
+        DataSet ds = new DataSet();
+
+        SqlDataAdapter adpt = new SqlDataAdapter("Select * from Student where Id="+ListBox1.SelectedValue, conn);
+
+        adpt.Fill(ds, "Student");
+
+        foreach (DataRow dr in ds.Tables["Student"].Rows)
+        {
+            txtStudName.Text = dr["Name"].ToString();
+            txtStudAddress.Text = dr["Address"].ToString();
+            txtContactNo.Text = dr["Contactno"].ToString();
+            txtStudEmail.Text = dr["Email"].ToString();
+        }
+        btnAdd.Visible = false;
+        btnUpdate.Visible = true;
+    }
+    protected void btnUpdate_Click(object sender, EventArgs e)
+    {
+
+        SqlConnection conn = new SqlConnection(@"Data Source=LAPTOP-35OT9IE8\SQLEXPRESS;Initial Catalog=webeng;Integrated Security=True");
+
+        DataSet ds = new DataSet();
+
+        SqlDataAdapter adpt = new SqlDataAdapter("Select * from Student where Id=" + ListBox1.SelectedValue, conn);
+
+        adpt.Fill(ds, "Student");
+
+        foreach (DataRow dr in ds.Tables["Student"].Rows)
+        {
+            dr["Name"] = txtStudName.Text;
+            dr["Address"] = txtStudAddress.Text;
+            dr["Contactno"] = txtContactNo.Text;
+            dr["Email"]=txtStudEmail.Text;
+        }
+        SqlCommandBuilder buil = new SqlCommandBuilder(adpt);
+        adpt.Update(ds, "Student");
+        refreshPage();
+
+    }
+    protected void btnDelete_Click(object sender, EventArgs e)
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source=LAPTOP-35OT9IE8\SQLEXPRESS;Initial Catalog=webeng;Integrated Security=True");
+
+        DataSet ds = new DataSet();
+
+        SqlDataAdapter adpt = new SqlDataAdapter("Select * from Student where Id=" + ListBox1.SelectedValue, conn);
+
+        adpt.Fill(ds, "Student");
+
+        foreach (DataRow dr in ds.Tables["Student"].Rows)
+        {
+            dr.Delete();
+        }
+        SqlCommandBuilder buil = new SqlCommandBuilder(adpt);
+        adpt.Update(ds, "Student");
+        GridView1.DataBind();
+        refreshPage();
+    }
+}
